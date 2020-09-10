363a68871c2f4756519cc55825dbd922440cde55
TPY <38260090+asd055189@users.noreply.github.com>
2020-09-10 19:50:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import urllib3
 import bs4
 HTTP = urllib3.PoolManager()
+account="帳號"
+passwd="密碼"
+keyword="你想報名的活動"
+login_page=HTTP.request("POST","https://portalx.yzu.edu.tw/PortalSocialVB/Login.aspx")
+VE=bs4.BeautifulSoup(login_page.data.decode(),'html.parser').select("#__VIEWSTATE")[0].get("value")
+VR=bs4.BeautifulSoup(login_page.data.decode(),'html.parser').select("#__VIEWSTATEGENERATOR")[0].get("value")
+EN=bs4.BeautifulSoup(login_page.data.decode(),'html.parser').select("#__EVENTVALIDATION")[0].get("value")
 login_page=HTTP.request("POST",
              "https://portalx.yzu.edu.tw/PortalSocialVB/Login.aspx",
               fields={
-                  "__VIEWSTATE":"/wEPDwUKLTkyMDk2NTcxNA9kFgICAw9kFgQCAQ8WAh4JaW5uZXJodG1sBfICPGRpdiBzdHlsZT0nY29sb3I6IHJlZDsgZm9udC13ZWlnaHQ6IGJvbGQ7Jz7luLPomZ/ngrpzIOWKoOS4iuaCqOeahOWtuOiZn++8jOWmgnM5MjExMDE8L2Rpdj48ZGl2IHN0eWxlPSdjb2xvcjogcmVkOyBmb250LXdlaWdodDogYm9sZDsnPuWvhueivOeCuiDouqvku73oqLzlrZfomZ8o6Iux5paH5a2X6KuL5aSn5a+rKTwvZGl2PjxkaXYgc3R5bGU9J2NvbG9yOiBibGFjazsnPueZu+WFpeW+jOiri+WLmeW/heS/ruaUueaCqOeahOWvhueivDwvZGl2PjxiciAvPjxkaXYgc3R5bGU9J2NvbG9yOiBibGFjazsnPuW/mOiomOWvhueivO+8muiri+mAlea0veWcluabuOmkqOarg+WPsCjlgpnorYnku7Yp77yM5oiW6Zu75qCh5YWn5YiG5qmfMjMyMTwvZGl2PmQCAw9kFgZmD2QWAmYPZBYCZg8PFgQeCENzc0NsYXNzBQ5Mb2dpbkJhY2s4MDBfWR4EXyFTQgICZBYCZg9kFgJmD2QWAgIBDw8WBB8BBQ5Mb2dpbkJhY2syNzBfWR8CAgJkFgJmD2QWAmYPZBYKAgEPDxYCHgRUZXh0BQbluLPomZ9kZAIFDw8WAh8DBQblr4bnorxkZAIJDw8WBB4HVG9vbFRpcAUPRW5nbGlzaCBWZXJzaW9uHghJbWFnZVVybAUcLi9JbWFnZXMvSWNvbnMvVmVyc2lvblRXLnBuZxYCHgdvbmNsaWNrBRVDaGFuZ2VMYW5ndWFnZSgnVFcnKTtkAgsPDxYCHwMFBueZu+WFpWRkAg0PDxYCHwMFDioq5paw55Sf5rOo5oSPZGQCAQ9kFgJmDw8WBB8BBQ90YWJDZWxsTWlkZGxlX1kfAgICZBYEAgMPDxYEHwEFDnRhYmxlSG90UGFnZV9ZHwICAmQWAmYPZBYCZg9kFgJmDw8WAh8DBQznhrHploDlsIjpoIFkZAIFDw8WBB8BBRF0YWJsZUhvdFBhZ2VFbmRfWR8CAgJkZAIDD2QWAmYPZBYCZg8PFgQfAQUNTG9naW5Gb290ZXJfWR8CAgJkZGTyT2gnXdKy6dcClhG2S9KvWwKitQ==",
-                  "__VIEWSTATEGENERATOR":"4F5352E6",
-                  "__EVENTVALIDATION":"/wEdAAYZE+NrQEMnXQZ2m08gbZJj1vGYFjrtzIuId5C42O0AzRaVgZp528BwL4Heg6ju5IFPJEHWQZAV39IrTgU3OMCuz9Epgz+OXGhhMky+n/+RyLga9dtgamGDPrZlwtr3L501mImYpIoFcNzIUq9OwA27skCHhA==",
-                  "Txt_UserID":'s+ID',
-                  "Txt_Password":'passwd',
+                  "__VIEWSTATE":VE,
+                  "__VIEWSTATEGENERATOR":VR,
+                  "__EVENTVALIDATION":EN,
+                  "Txt_UserID":account,
+                  "Txt_Password":passwd,
                   "ibnSubmit":"登入"
               })
 portal_x_key = [i for i in login_page.headers["Set-Cookie"].split(", ") if i.find("ASP.NET") != -1][0].split(";")[0].split("=")[1]
@@ -21,7 +28,7 @@ for i in bs4.BeautifulSoup(Activitypage,'html.parser').select("a"):
         activitycode.append(num)
 for i in range(0,len(activitycode)) :
     index=HTTP.request("POST","https://portalx.yzu.edu.tw/PortalSocialVB/FPage/PageActivityDetail.aspx?Menu=Act&ActID="+activitycode[i],headers={"Cookie":"ASP.NET_SessionId="+portal_x_key,"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.2 Safari/605.1.15"}).data.decode("utf-8")
-    if str(bs4.BeautifulSoup(index,'html.parser').select("#divActivityDetail")).find("keyword") > 0 :
+    if str(bs4.BeautifulSoup(index,'html.parser').select("#divActivityDetail")).find(keyword) > 0 :
         print("https://portalx.yzu.edu.tw/PortalSocialVB/FPage/PageActivityDetail.aspx?Menu=Act&ActID="+activitycode[i])
         pos=str(bs4.BeautifulSoup(index,'html.parser').select("#divActivityDetail")).find("上限")+3
         up=""
